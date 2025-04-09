7b15bfdc3c2953bae764bc36d3c063e718f407d3
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-09 17:34:06 +0530

    basic structural ui of hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
-        <div className="paddings innerWidth flexCenter hero-container">
+      <div className="paddings innerWidth flexCenter hero-container">
 
-            <div className="hero-left">
-                left section
-            </div>
-
-            <div className="flexCenter hero-right">
-                <div className="image-container">
-                    <img src="./hero-image.png" alt="hero png" />
-                </div>
+        {/* hero left side */}
+        <div className="hero-left">
+            <div className="hero-title">
+                <h1>Discover <br/>
+                Most Suitable <br/>
+                Property
+                </h1>
             </div>
         </div>
-    </section>
-  )
-}
 
-export default Hero
+        {/* hero right */}
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="hero png" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
