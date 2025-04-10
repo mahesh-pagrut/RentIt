3e2fd5358fbc06dfb0410b4dc3459f48679ef803
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-10 19:34:38 +0530

    slider done

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -3,6 +3,7 @@ import "./Residencies.css";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/common";
 
 const Residencies = () => {
   return (
@@ -13,7 +14,7 @@ const Residencies = () => {
           <span className="primaryText">Popular Residencies</span>
         </div>
 
-        <Swiper>
+        <Swiper {...sliderSettings}>
           {data.map((card, i) => (
             <SwiperSlide key={i}>
               <div className="flexColStart r-card">
