e25fbe3f3492881aec2fc2e084000377e05844c8
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-10 19:10:11 +0530

    slides setting in residence and ui

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -16,12 +16,15 @@ const Residencies = () => {
         <Swiper>
           {data.map((card, i) => (
             <SwiperSlide key={i}>
-              <div className="r-card">
+              <div className="flexColStart r-card">
                 <img src={card.image} alt="home" />
                 <span className="secondaryText r-price">
                   <span>$</span>
                   <span>{card.price}</span>
                 </span>
+
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
               </div>
             </SwiperSlide>
           ))}
