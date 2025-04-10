289b541b652865c9e44113afbaaf0e41dfc7acc1
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-10 14:21:14 +0530

    done hero ui and structure content with count up text

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
           </div>
 
           <div className="flexColStart hero-des">
-            <span>Find a variety of properties that suit you</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">Find a variety of properties that suit you</span>
+            <span className="secondaryText">Forget all difficulties in finding a residence for you</span>
           </div>
 
           <div className="flexCenter search-bar">
@@ -30,16 +30,34 @@ const Hero = () => {
           </div>
 
           <div className="flexCenter stats">
-            <div className="flexColStart stat">
+            <div className="flexColCenter stat">
                 <span>
                     <CountUp start={8800} end={9000} duration={4}/>
                     <span>+</span>
                 </span>
+                <span className="secondaryText">Premium Products</span>
             </div>
+
+            <div className="flexColCenter stat">
+                <span>
+                    <CountUp start={1950} end={2000} duration={4}/>
+                    <span>+</span>
+                </span>
+                <span className="secondaryText">Happy Customers</span>
+            </div>
+
+            <div className="flexColCenter stat">
+                <span>
+                    <CountUp end={28}/>
+                    <span>+</span>
+                </span>
+                <span className="secondaryText"> Award Winning</span>
+            </div>
+
           </div>
         </div>
 
-        {/* hero right */}
+        {/* hero right side */}
         <div className="flexCenter hero-right">
           <div className="image-container">
             <img src="./hero-image.png" alt="hero png" />
