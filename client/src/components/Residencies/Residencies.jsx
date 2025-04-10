709386e4ea61804dbf57cf87fb5654216a97bd89
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-10 23:58:10 +0530

    fixed slider buttons

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Residencies.css";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import data from "../../utils/slider.json";
 import { sliderSettings } from "../../utils/common";
 
 const Residencies = () => {
+  const [swiperInstance, setSwiperInstance] = useState(null); // <== Store swiper
+
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -14,23 +16,26 @@ const Residencies = () => {
           <span className="primaryText">Popular Residencies</span>
         </div>
 
-        <Swiper {...sliderSettings}>
-            <SliderButtons/>
-          {data.map((card, i) => (
-            <SwiperSlide key={i}>
-              <div className="flexColStart r-card">
-                <img src={card.image} alt="home" />
-                <span className="secondaryText r-price">
-                  <span>$</span>
-                  <span>{card.price}</span>
-                </span>
+        <div className="r-swiper-container">
+          <Swiper {...sliderSettings} onSwiper={setSwiperInstance}>
+            {data.map((card, i) => (
+              <SwiperSlide key={i}>
+                <div className="flexColStart r-card">
+                  <img src={card.image} alt="home" />
+                  <span className="secondaryText r-price">
+                    <span>$</span>
+                    <span>{card.price}</span>
+                  </span>
+                  <span className="primaryText">{card.name}</span>
+                  <span className="secondaryText">{card.detail}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
 
-                <span className="primaryText">{card.name}</span>
-                <span className="secondaryText">{card.detail}</span>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+          {/* Pass swiper instance to buttons */}
+          {swiperInstance && <SliderButtons swiper={swiperInstance} />}
+        </div>
       </div>
     </section>
   );
@@ -38,12 +43,12 @@ const Residencies = () => {
 
 export default Residencies;
 
-const SliderButtons = () =>{
-    const swiper = useSwiper();
-    return (
-        <div className="flexCenter r-buttons">
-            <button onClick={() => swiper.slidePrev()}>&lt;</button>
-            <button onClick={() => swiper.slideNext()}>&gt;</button>
-        </div>
-    )
-}
+// Modify this to accept `swiper` as a prop
+const SliderButtons = ({ swiper }) => {
+  return (
+    <div className="r-buttons">
+      <button onClick={() => swiper.slidePrev()}>&lt;</button>
+      <button onClick={() => swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
