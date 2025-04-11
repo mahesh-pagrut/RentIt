283bb79cc5e34296cddebd410379ed20037554f1
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-12 01:51:51 +0530

    contact component structure jsx

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import './Contact.css'
-
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="flexColStart c-left">
+          <span className="orangeText">Contact Us</span>
+          <span className="primaryText">We’re Here to Help</span>
+          <span className="secondaryText">
+            Have questions or need support? Reach out anytime — we’re always
+            ready to assist you and provide the best rental experience possible.
+          </span>
+        </div>
 
-export default Contact
+        {/* right side */}
+
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="contact" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
