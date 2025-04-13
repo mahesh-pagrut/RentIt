70c54d88b4687f388cae53c4d01cd3839f31cef0
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-13 10:26:43 +0530

    fixed text

--- client/src/components/GetStarted/GetStarted.jsx
+++ client/src/components/GetStarted/GetStarted.jsx
@@ -1,19 +1,20 @@
-
-import React from 'react'
-import './GetStarted.css'
+import React from "react";
+import "./GetStarted.css";
 
 const GetStarted = () => {
   return (
     <section className="g-wrapper">
-        <div className="paddings innerWidth g-container">
-            <div className="flexColCenter inner-container">
-                <span className='primaryText'>Get started with the RentIt</span>
-                <span className='secondaryText'>Subscribe and find super attractive price quotes <br /> find your next residence</span>
-                
-            </div>
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started with RentIt</span>
+          <span className="secondaryText">
+            Subscribe now to explore unbeatable rental deals <br />
+            and discover your next perfect residence.
+          </span>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default GetStarted
+export default GetStarted;
