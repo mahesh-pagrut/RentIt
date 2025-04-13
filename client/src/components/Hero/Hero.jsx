808f7116cb07bcef5fcd558da5981832f3fef3a6
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-13 20:56:57 +0530

    framer motion

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
-import CountUp from "react-countup"
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -12,58 +13,86 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+
+            <motion.h1
+              initial={{ y: 60, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1, type: "spring", delay: 0.2 }}
+            >
               Discover <br />
               Most Suitable <br />
               Property
-            </h1>
+            </motion.h1>
           </div>
 
-          <div className="flexColStart hero-des">
-            <span className="secondaryText">Find a variety of properties that suit you</span>
-            <span className="secondaryText">Forget all difficulties in finding a residence for you</span>
-          </div>
+          <motion.div
+            className="flexColStart hero-des"
+            initial={{ y: 40, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.4 }}
+          >
+            <span className="secondaryText">
+              Find a variety of properties that suit you
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
+          </motion.div>
 
-          <div className="flexCenter search-bar">
+          <motion.div
+            className="flexCenter search-bar"
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.6, type: "spring" }}
+          >
             <HiLocationMarker color="var(--blue)" size={25} />
-            <input type="text" />
+            <input type="text" placeholder="Search location..." />
             <button className="button">Search</button>
-          </div>
+          </motion.div>
 
-          <div className="flexCenter stats">
+          <motion.div
+            className="flexCenter stats"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.8 }}
+          >
             <div className="flexColCenter stat">
-                <span>
-                    <CountUp start={8800} end={9000} duration={4}/>
-                    <span>+</span>
-                </span>
-                <span className="secondaryText">Premium Products</span>
+              <span>
+                <CountUp start={8800} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
             </div>
 
             <div className="flexColCenter stat">
-                <span>
-                    <CountUp start={1950} end={2000} duration={4}/>
-                    <span>+</span>
-                </span>
-                <span className="secondaryText">Happy Customers</span>
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
             </div>
 
             <div className="flexColCenter stat">
-                <span>
-                    <CountUp end={28}/>
-                    <span>+</span>
-                </span>
-                <span className="secondaryText"> Award Winning</span>
+              <span>
+                <CountUp end={28} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winning</span>
             </div>
-
-          </div>
+          </motion.div>
         </div>
 
         {/* hero right side */}
-        <div className="flexCenter hero-right">
+        <motion.div
+          className="flexCenter hero-right"
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1.2, delay: 0.5, type: "spring" }}
+        >
           <div className="image-container">
-            <img src="./hero-image.png" alt="hero png" />
+            <img src="./hero-image.png" alt="hero" />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
