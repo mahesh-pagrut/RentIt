919d968c9cd6ffdcb17978018306be2b542d697b
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-13 12:13:15 +0530

    jsx footer done structure

--- client/src/components/Fooder/Footer.jsx
+++ client/src/components/Fooder/Footer.jsx
@@ -3,17 +3,32 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="paddings innerWidth flexCenter f-container">
-      {/* left side */}
-      <div className="flexColStart f-left">
-        <img src="./logo.png" alt="" width={120} />
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* left side */}
+        <div className="flexColStart f-left">
+          <img src="./logo.png" alt="" width={120} />
 
-        <span className="secondaryText">
-          Our vision is to help everyone find <br />
-          the perfect place to call home.
-        </span>
+          <span className="secondaryText">
+            Our vision is to help everyone find <br />
+            the perfect place to call home.
+          </span>
+        </div>
+
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">
+            Sector 45, Gurugram , Delhi NCR, INDIA
+          </span>
+          <div className="flexCenter f-menu">
+            <span>Property</span
+            ><span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
